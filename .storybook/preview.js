0f92d5b141a4a9a8e2719e3a983929225c85c442
Ashur Cabrera <hello@ashur.cab>
2022-01-24 08:38:22 -0700

    Add --background-color to backgrounds addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,13 @@ import '../src/styles/global/vars.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    values: [
+      { name: '--background-color', value: 'var(--background-color)' },
+      { name: 'light', value: '#f8f8f8' },
+      { name: 'blue', value: '#333' },
+    ],
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
